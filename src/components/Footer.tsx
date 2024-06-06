25e431b7b23e2fe28cff9fa1150a31c0884ed50a
xdjeremy <44615921+xdjeremy@users.noreply.github.com>
2024-06-06 20:16:33 +0800

    TIM-242 feat(layout): add desktop footer (#11)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,56 +10,66 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <div className="w-full">
-      <div className="bg-[#947901] pt-9 pb-10 flex flex-col items-center justify-center  gap-6">
-        <h4 className="text-white font-bold text-sm">Contacts</h4>
-        <div className="flex items-center justify-center flex-col text-white">
-          <MapPin className="w-4 h-4" />
-          <span className="w-56 text-center">
-            6B Isaac Tolentino Ave, Brgy. San Jose, Tagaytay City, Philippines
-          </span>
-        </div>
-        <div className="flex items-center justify-center flex-col text-white">
-          <Phone className="w-4 h-4" />
-          <span className="w-56 text-center">
-            6B Isaac Tolentino Ave, Brgy. San Jose, Tagaytay City, Philippines
-          </span>
-        </div>
-        <div className="flex items-center justify-center flex-col text-white">
-          <Mail className="w-4 h-4" />
-          <span className="w-56 text-center">
-            6B Isaac Tolentino Ave, Brgy. San Jose, Tagaytay City, Philippines
-          </span>
-        </div>
-        <div className="text-center">
-          <h3 className="text-white font-bold text-sm">Subscribe</h3>
-          <p className="text-white text-xs w-64">
-            Stay updated with our latest news and offers by subscribing to our
-            Newsletter.
-          </p>
+      <div className="bg-[#947901] pt-9 pb-10 flex flex-col items-center justify-center md:flex-row md:justify-between md:items-start md:px-28">
+        <div className="flex flex-col gap-6">
+          <h4 className="text-white font-bold text-sm text-center md:text-2xl md:font-bold md:text-left">
+            Contacts
+          </h4>
+          <div className="flex items-center md:flex-row justify-center md:items-start md:gap-5 flex-col text-white">
+            <MapPin className="w-4 h-4 md:translate-y-1" />
+            <span className="w-56 text-center md:text-left md:w-44">
+              6B Isaac Tolentino Ave, Brgy. San Jose, Tagaytay City, Philippines
+            </span>
+          </div>
+          <div className="flex items-center md:flex-row justify-center md:items-start md:gap-5 flex-col text-white">
+            <Phone className="w-4 h-4 md:translate-y-1" />
+            <span className="w-56 text-center md:text-left md:w-44">
+              0917 122 0441
+            </span>
+          </div>
+          <div className="flex items-center md:flex-row justify-center md:items-start md:gap-5 flex-col text-white">
+            <Mail className="w-4 h-4 md:translate-y-1" />
+            <span className="w-56 text-center md:text-left md:w-44">
+              fo.tagaytayhotelsixb@gmail.com
+            </span>
+          </div>
         </div>
 
-        <div className="relative">
-          <Input className="rounded-full w-64" placeholder="Email" />
-          <Button
-            className="rounded-full bg-[#6F5300] text-white absolute right-0 top-0"
-            size={"icon"}
-          >
-            <ArrowRight className="w-6 h-6" />
-          </Button>
+        <div className="mt-6 md:mt-0 flex flex-col gap-6">
+          <div className="text-center md:text-left">
+            <h4 className="text-white font-bold text-sm text-center md:text-2xl md:font-bold md:text-left">
+              Subscribe
+            </h4>
+            <p className="text-white text-xs w-64 md:w-56 md:mt-5">
+              Stay updated with our latest news and offers by subscribing to our
+              Newsletter.
+            </p>
+          </div>
+          <div className="relative">
+            <Input className="rounded-full w-64" placeholder="Email" />
+            <Button
+              className="rounded-full bg-[#6F5300] text-white absolute right-0 top-0"
+              size={"icon"}
+            >
+              <ArrowRight className="w-6 h-6" />
+            </Button>
+          </div>
+          <div className="flex flex-row items-center justify-center md:justify-start md:pl-5">
+            <Button variant={"ghost"} size={"icon"}>
+              <FooterFacebookIcon className="text-white" />
+            </Button>
+            <Button variant={"ghost"} size={"icon"}>
+              <FooterInstagramIcon className="text-white" />
+            </Button>
+          </div>
         </div>
 
-        <div className="flex flex-row items-center justify-center">
-          <Button variant={"ghost"} size={"icon"}>
-            <FooterFacebookIcon className="text-white" />
-          </Button>
-          <Button variant={"ghost"} size={"icon"}>
-            <FooterInstagramIcon className="text-white" />
-          </Button>
+        <div className="flex flex-col items-center justify-center h-full">
+          <Image src={Logo} alt="Tagaytay Hotel SixB" width={191} height={38} />
         </div>
-        <Image src={Logo} alt="Tagaytay Hotel SixB" width={191} height={38} />
       </div>
-      <div className="bg-[#774E17] w-full flex flex-row items-center justify-center py-2.5">
-        <p className="text-white text-xs uppercase">
+      <div className="bg-[#774E17] w-full flex flex-row items-center justify-center py-2.5 md:justify-start">
+        <p className="text-white text-xs uppercase max-w-5xl w-ful px-12">
           &copy; 2024 Tagaytay Hotel SixB. All rights reserved
         </p>
       </div>
