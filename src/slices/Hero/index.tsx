d040ced9fdd291d44c5c4cddd732894a68f6b32d
tamrivera05 <tamangelarivera@gmail.com>
2024-07-05 03:03:41 +0000

    TIM-322 Refactor video on homepage (#63)

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,8 +1,6 @@
 'use client'
 import {Content} from "@prismicio/client";
 import {SliceComponentProps} from "@prismicio/react";
-import Script from "next/script";
-import {PrismicNextLink} from "@prismicio/next";
 import React from "react";
 
 /**
@@ -19,8 +17,8 @@ const Hero = ({slice}: HeroProps): JSX.Element => {
             data-slice-type={slice.slice_type}
             data-slice-variation={slice.variation}
         >
-           <video playsInline autoPlay muted loop preload="auto" className="w-full object-cover md:h-[662px] ">
-                <source src="https://vzooaoblxhvxmkjxpeve.supabase.co/storage/v1/object/public/media/youtube_9rCtHliJAec_1280x720_h264.mp4?t=2024-07-02T06%3A41%3A11.436Z"
+           <video playsInline autoPlay muted loop preload="auto" className="w-full object-cover md:h-[662px] " preload="auto">
+                <source src="https://vzooaoblxhvxmkjxpeve.supabase.co/storage/v1/object/public/media/youtube_9rCtHliJAec_1280x720_h264.mp4?t=2024-07-02T06%3A41%3A11.436Z#t=3"
                         type="video/mp4"/>
            </video>
         </section>
