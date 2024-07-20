8cf566596d1d2504e72667afe87de0a49721d4be
xdjeremy <jeremy.fran@icloud.com>
2024-07-20 13:53:18 +0000

    fix(hero): duplicate props eslint error (#66)

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -17,7 +17,7 @@ const Hero = ({slice}: HeroProps): JSX.Element => {
             data-slice-type={slice.slice_type}
             data-slice-variation={slice.variation}
         >
-           <video playsInline autoPlay muted loop preload="auto" className="w-full object-cover md:h-[662px] " preload="auto">
+           <video playsInline autoPlay muted loop preload="auto" className="w-full object-cover md:h-[662px] ">
                 <source src="https://vzooaoblxhvxmkjxpeve.supabase.co/storage/v1/object/public/media/youtube_9rCtHliJAec_1280x720_h264.mp4?t=2024-07-02T06%3A41%3A11.436Z#t=3"
                         type="video/mp4"/>
            </video>
