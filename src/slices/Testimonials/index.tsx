c4d987000952d51fb1955b4b0ca706a9525b87ed
tamrivera05 <tamangelarivera@gmail.com>
2024-07-30 03:34:57 +0000

    fix: Carousel Indicators (#70)

--- src/slices/Testimonials/index.tsx
+++ src/slices/Testimonials/index.tsx
@@ -1,6 +1,8 @@
+'use client'
+
 import {Content} from "@prismicio/client";
 import {SliceComponentProps} from "@prismicio/react";
-import {Carousel, CarouselApi, CarouselContent, CarouselItem} from "@/components/ui/carousel";
+import {Carousel, CarouselApi, CarouselContent, CarouselDots, CarouselItem} from "@/components/ui/carousel";
 
 /**
  * Props for `Testimonials`.
@@ -11,22 +13,6 @@ export type TestimonialsProps = SliceComponentProps<Content.TestimonialsSlice>;
  * Component for "Testimonials" Slices.
  */
 const Testimonials = ({slice}: TestimonialsProps): JSX.Element => {
-    // const [api, setApi] = useState<CarouselApi>();
-    // const [current, setCurrent] = useState(0);
-    // const [count, setCount] = useState(0);
-    //
-    // useEffect(() => {
-    //     if (!api) {
-    //         return;
-    //     }
-    //
-    //     setCount(api.scrollSnapList().length);
-    //     setCurrent(api.selectedScrollSnap() + 1);
-    //
-    //     api.on('select', () => {
-    //         setCurrent(api.selectedScrollSnap() + 1);
-    //     });
-    // }, [api]);
     return (
         <section
             data-slice-type={slice.slice_type}
@@ -64,16 +50,7 @@ const Testimonials = ({slice}: TestimonialsProps): JSX.Element => {
                             </div>
                         </CarouselItem>
                     </CarouselContent>
-                    {/*<div className="flex flex-row justify-center mt-4">*/}
-                    {/*    {Array.from({length: count}).map((_, index) => (*/}
-                    {/*        <span*/}
-                    {/*            key={index}*/}
-                    {/*            className={cn(index + 1 === current ? 'bg-gray-800' : 'bg-gray-400', ' w-3 h-3 rounded-full mx-2')}*/}
-                    {/*            onClick={() => api && api.scrollTo(index)}*/}
-                    {/*        />*/}
-                    {/*    ))}*/}
-                    {/*</div>*/}
-                    <CarouselDots classname="mt-4"/>
+                    <CarouselDots className="mt-4"/>
                 </Carousel>
             </div>
 
